4d0b30737428337f11cee78e65f6efb9e640c8dd
rameshjayaraj <rameshjayaraj@gmail.com>
2021-06-12 19:49:29 +0530

    Users Service - Eureka Discovery Client

---
+++ src/test/java/com/mobileapp/users/MobileappUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobileapp.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileappUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
